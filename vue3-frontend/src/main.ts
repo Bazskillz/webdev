aa94c270d6759b20b6e266c172b425f2328ceb37
Bazskillz <bas-daniel2@hotmail.com>
2023-04-07 22:29:52 +0200

    vue3-datepicker

--- vue3-frontend/src/main.ts
+++ vue3-frontend/src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import navbar from './components/navbar.vue';
-
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 import './index.css'
 
 const app = createApp(App)
+app.component('VueDatePicker', VueDatePicker);
 
 app.use(router)
 app.mount('#app')
--- vue3-frontend/src/views/Calendar.vue
+++ vue3-frontend/src/views/Calendar.vue
@@ -9,14 +9,15 @@
               <p class="text-gray-700 font-medium">{{ event.summary }}</p>
               <div class="grid grid-cols-2 gap-4 mt-4">
                 <div class="text-gray-500"><span class="font-medium">Location:</span> {{ event.location }}</div>
-                <div class="text-gray-500"><span class="font-medium">Start Time:</span> {{ event.start.dateTime ? new Date(event.start.dateTime).toLocaleString() : event.start.date }}</div>
-                <div class="text-gray-500"><span class="font-medium">End Time:</span> {{ event.end.dateTime ? new Date(event.end.dateTime).toLocaleString() : event.end.date }}</div>
+                <div class="text-gray-500"><span class="font-medium">Start Time:</span> {{ event.start.dateTime ? new Date(event.start.dateTime).toLocaleString('en-US', {timeZone: 'UTC', hour12: false, hour: 'numeric', minute: 'numeric'}) : event.start.date }}</div>
+                <div class="text-gray-500"><span class="font-medium">End Time:</span> {{ event.end.dateTime ? new Date(event.end.dateTime).toLocaleString('en-US', {timeZone: 'UTC', hour12: false, hour: 'numeric', minute: 'numeric'}) : event.end.date }}</div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <form class="mt-4 px-8 py-4 bg-white rounded-lg shadow-md">
+      <button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 mt-4" @click.prevent="showForm = !showForm">{{ showForm ? 'Hide' : 'Add New Item' }}</button>
+      <form v-if="showForm" class="mt-4 px-8 py-4 bg-white rounded-lg shadow-md">
         <h3 class="text-lg font-medium mb-4">New Event</h3>
         <div class="mb-4">
           <label class="block text-gray-700 font-medium mb-2" for="summary">Summary</label>
@@ -28,11 +29,11 @@
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 font-medium mb-2" for="start-time">Start Time</label>
-          <input class="border border-gray-400 p-2 w-full rounded-lg" type="datetime-local" id="start-time" v-model="newEvent.start.dateTime">
+          <VueDatePicker class="border border-gray-400 p-2 w-full rounded-lg" id="start-time" v-model="newEvent.start.dateTime"/>
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 font-medium mb-2" for="end-time">End Time</label>
-          <input class="border border-gray-400 p-2 w-full rounded-lg" type="datetime-local" id="end-time" v-model="newEvent.end.dateTime">
+          <VueDatePicker class="border border-gray-400 p-2 w-full rounded-lg" id="end-time" v-model="newEvent.end.dateTime"/>
         </div>
         <button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600" @click.prevent="addEvent()">Add Event</button>
       </form>
@@ -43,6 +44,11 @@
 <script>
 import axios from 'axios';
 import Navbar from '../components/navbar.vue';
+import { ref } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+
 
 export default {
   components: {
@@ -50,6 +56,7 @@ export default {
   },
   data() {
   return {
+    showForm: false,
     selectedDate: new Date(),
     jsonData: [],
     newEvent: {
@@ -61,7 +68,8 @@ export default {
       end: {
         dateTime: ""
       }
-    }
+    },
+    dateFormat: 'dd-MM-yyyyTHH:mm'
   }
 },
   mounted() {
